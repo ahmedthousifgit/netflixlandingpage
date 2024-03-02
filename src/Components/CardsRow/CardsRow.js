2cd3b93ac66f53a13907dd57add2c15129f580ec
ahmed thousif <ahmedthousif571@gmail.com>
2024-03-02 15:57:38 +0530

    completed almost

--- src/Components/CardsRow/CardsRow.js
+++ src/Components/CardsRow/CardsRow.js
@@ -1,17 +1,38 @@
 import "./CardsRow.css";
 import axios from "../../axios";
 import React, { useEffect, useState } from "react";
-import {  imageUrl } from "../../constants/constants";
+import { imageUrl, API_KEY } from "../../constants/constants";
+import YouTube from "react-youtube";
 function CardsRow(props) {
   const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
   useEffect(() => {
-    axios
-      .get(props.url)
-      .then((response) => {
-        setMovies(response.data.results);
-      });
-  }, []);
+    axios.get(props.url).then((response) => {
+      setMovies(response.data.results);
+    });
+  });
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) => {
+    console.log(id);
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("array is empty");
+        }
+      });
+  };
   return (
     <div className="row">
       <h2>{props.title}</h2>
@@ -19,6 +40,7 @@ function CardsRow(props) {
         {movies.map((obj) => {
           return (
             <img
+              onClick={() => handleMovie(obj.id)}
               className={props.isSmall ? "smallPoster" : "poster"}
               src={`${imageUrl + obj.backdrop_path}`}
               alt="poster"
@@ -26,6 +48,7 @@ function CardsRow(props) {
           );
         })}
       </div>
+      {urlId && <YouTube opts={opts} videoId={urlId.key} />}
     </div>
   );
 }
