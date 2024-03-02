1a58b7cbc78d4333b0b96bcc017d4dbdbd82039f
ahmed thousif <ahmedthousif571@gmail.com>
2024-03-02 16:54:53 +0530

    netflix completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import './App.css';
-import Banner from './Components/Banner/Banner';
-import Nav from './Components/NavBar/Nav';
-import {Orginals,action} from './urls'
-import './Components/NavBar/Nav.css'
-import './Components/Banner/Banner.css'
-import CardsRow from './Components/CardsRow/CardsRow';
-import './Components/CardsRow/CardsRow.css'
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import Nav from "./Components/NavBar/Nav";
+import { Orginals, action, comedy, horror, romance,documentaries } from "./urls";
+import "./Components/NavBar/Nav.css";
+import "./Components/Banner/Banner.css";
+import CardsRow from "./Components/CardsRow/CardsRow";
+import "./Components/CardsRow/CardsRow.css";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Banner/>
-      <CardsRow url={Orginals}  title="Netflix Orginals" imageUrl=""/>
-      <CardsRow  url={action} title="Action" isSmall  />
+      <Nav />
+      <Banner />
+      <CardsRow url={Orginals} title="Netflix Orginals" imageUrl="" />
+      <CardsRow url={action} title="Action" isSmall />
+      <CardsRow url={comedy} title="Comedy" isSmall />
+      <CardsRow url={horror} title="Horror" isSmall />
+      <CardsRow url={romance} title="Romance" isSmall />
+      <CardsRow url={documentaries} title="Documentary" isSmall />
+
     </div>
   );
 }
